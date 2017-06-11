1f1a968aa7eae1bda92de53f6e6eca2d1d3adc39
Simon Müller <simon.f.mueller@gmail.com>
2017-06-11 18:04:48 +0200

    Create site.mk

--- site.mk
+++ site.mk
@@ -1,8 +1,5 @@
-##	GLUON_SITE_PACKAGES
-#		specify gluon/openwrt packages to include here
-#		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
-
 GLUON_SITE_PACKAGES := \
+gluon-mesh-batman-adv-14 \
 gluon-autoupdater \
 gluon-config-mode-autoupdater \
 gluon-config-mode-contact-info \
@@ -12,7 +9,6 @@ gluon-config-mode-hostname \
 gluon-config-mode-mesh-vpn \
 gluon-ebtables-filter-multicast \
 gluon-ebtables-filter-ra-dhcp \
-gluon-mesh-batman-adv-14 \
 gluon-mesh-vpn-fastd \
 gluon-neighbour-info \
 gluon-radvd \
@@ -29,20 +25,15 @@ iptables \
 iwinfo
 
 # Firmware name pattern
-DEFAULT_GLUON_RELEASE := 2017.1-exp$(shell date '+%Y%m%d')
-
-##	GLUON_RELEASE
-#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
-#		e.g.:
-#			$ make images GLUON_RELEASE=23.42+5
-#		would generate images named like this:
-#			gluon-ff%site_code%-23.42+5-%router_model%.bin
+DEFAULT_GLUON_RELEASE := 2017.1-lede$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 # Default priority for updates.
 GLUON_PRIORITY ?= 0
+GLUON_BRANCH ?= lede
+export GLUON_BRANCH
 
 # Languages to include
 GLUON_LANGS ?= en de
@@ -50,4 +41,4 @@ GLUON_LANGS ?= en de
 # Region settings for ARCHERC7
 GLUON_REGION ?= eu
 
-GLUON_ATH10K_MESH = ibss
+GLUON_ATH10K_MESH ?= ibss
