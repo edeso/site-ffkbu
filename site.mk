39b88bbd2e45a53ee182929a3cd4074c1253d573
Simon Müller <simon.f.mueller@gmail.com>
2017-06-18 16:45:15 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,29 +1,28 @@
 GLUON_SITE_PACKAGES := \
-gluon-mesh-batman-adv-14 \
-gluon-alfred \
-gluon-autoupdater \
-gluon-config-mode-autoupdater \
-gluon-config-mode-contact-info \
-gluon-config-mode-core \
-gluon-config-mode-geo-location \
-gluon-config-mode-hostname \
-gluon-config-mode-mesh-vpn \
-gluon-ebtables-filter-multicast \
-gluon-ebtables-filter-ra-dhcp \
-gluon-mesh-vpn-fastd \
-gluon-neighbour-info \
-gluon-radvd \
-gluon-respondd \
-gluon-setup-mode \
-gluon-status-page \
-gluon-web-admin \
-gluon-web-autoupdater \
-gluon-web-mesh-vpn-fastd \
-gluon-web-network \
-gluon-web-wifi-config \
-haveged \
-iptables \
-iwinfo
+  gluon-mesh-batman-adv-14 \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-ebtables-segment-mld \
+	gluon-web-admin \
+	gluon-web-autoupdater \
+	gluon-web-network \
+	gluon-web-wifi-config \
+	gluon-web-private-wifi \
+	gluon-web-node-role \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	haveged \
+	iptables \
+	iwinfo
 
 # Firmware name pattern
 DEFAULT_GLUON_RELEASE := 2017.1-lede$(shell date '+%Y%m%d')
@@ -32,7 +31,7 @@ DEFAULT_GLUON_RELEASE := 2017.1-lede$(shell date '+%Y%m%d')
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 # Default priority for updates.
-GLUON_PRIORITY ?= 0
+GLUON_PRIORITY := 0
 GLUON_BRANCH ?= lede
 export GLUON_BRANCH
 
